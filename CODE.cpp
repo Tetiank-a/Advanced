1ff9f57358c9fad8f77d16b70d7a3175a2fb3e98
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-10-22 19:46:53 +0300

    Update CODE.cpp

--- CODE.cpp
+++ CODE.cpp
@@ -7,12 +7,15 @@ const int64_t kMd = 1000000007;
 
 struct Edge
 {
-    int64_t room_from;
-    int64_t room_to;
+    int64_t vertex_from;
+    int64_t vertex_to;
 };
 
+// Reading numbers
+int64_t ReadNumber(std::istream& input = std::cin);
+
 // Creating and reading edges
-std::vector <Edge> ReadEdges(int64_t number_of_paths,
+std::vector <Edge> ReadEdges(int64_t number_of_edges,
                              std::istream& input = std::cin);
 
 // Writing a number of possible variants
@@ -24,8 +27,9 @@ class Matrix {
     int64_t size_;
  public:
     int64_t GetSize();
-    Matrix(int64_t number_of_rooms, int64_t digit);
-    void Create(std::vector <Edge> edges, int64_t number_of_path);
+    Matrix(int64_t matrix_size, int64_t digit);
+    Matrix(std::vector <Edge> edges, int64_t matrix_size,
+           int64_t number_of_edges);
     int64_t get(int64_t i, int64_t j);
     void set(int64_t i, int64_t j, int64_t value);
 };
@@ -35,19 +39,25 @@ int64_t Matrix::GetSize() {
 }
 
 // Creating an identity matrix N*N
-Matrix::Matrix(int64_t number_of_rooms, int64_t digit) {
-    std::vector <int64_t> str(number_of_rooms, 0);
-    for (int64_t i = 0; i < number_of_rooms; ++i)
+Matrix::Matrix(int64_t matrix_size, int64_t digit) {
+    std::vector <int64_t> str(matrix_size, 0);
+    for (int64_t i = 0; i < matrix_size; ++i)
         this->matrix_.push_back(str);
-    for (int64_t i = 0; i < number_of_rooms; ++i)
+    for (int64_t i = 0; i < matrix_size; ++i)
         this->matrix_[i][i] = digit;
-    this->size_ = number_of_rooms;
+    this->size_ = matrix_size;
 }
 
-void Matrix::Create(std::vector <Edge> edges, int64_t number_of_path) {
+Matrix::Matrix(std::vector <Edge> edges, int64_t matrix_size,
+               int64_t number_of_edges) {
+    this->size_ = matrix_size;
+    std::vector <int64_t> str(matrix_size, 0);
+    for (int64_t i = 0; i < matrix_size; ++i)
+        this->matrix_.push_back(str);
+
     int64_t x, y;
-    for (int64_t i = 0; i < number_of_path; ++i) {
-        this->matrix_[edges[i].room_from - 1][edges[i].room_to - 1]++;
+    for (int64_t i = 0; i < number_of_edges; ++i) {
+        this->matrix_[edges[i].vertex_from - 1][edges[i].vertex_to - 1]++;
     }
 }
 
@@ -87,37 +97,35 @@ Matrix MatrixInPower(Matrix a, int64_t k) {
 }
 
 // The number of possible paths from room#0 with given length
-int64_t SumCalc(Matrix a, int64_t number_of_rooms) {
+int64_t SumCalculation(Matrix matrix, int64_t matrix_size) {
     int64_t sum = 0;
-    for (int64_t i = 0; i < number_of_rooms; ++i)
-        sum = (sum + a.get(0, i)) % kMd;
+    for (int64_t i = 0; i < matrix_size; ++i)
+        sum = (sum + matrix.get(0, i)) % kMd;
     return sum;
 }
 
-int64_t Run(int64_t number_of_rooms, int64_t number_of_path,
+int64_t CountNumberOfVariants(int64_t number_of_rooms, int64_t number_of_edges,
             int64_t path_length, std::vector <Edge> edges) {
-    Matrix rooms_matrix(number_of_rooms, 0);
-    rooms_matrix.Create(edges, number_of_path);
+    Matrix rooms_matrix(edges, number_of_rooms, number_of_edges);
     rooms_matrix = MatrixInPower(rooms_matrix, path_length);
-    return SumCalc(rooms_matrix, number_of_rooms);
+    return SumCalculation(rooms_matrix, number_of_rooms);
 }
 
-int64_t ReadNumber(std::istream& input = std::cin);
 int64_t ReadNumber(std::istream& input) {
     int value;
     input >> value;
     return value;
 }
 
-std::vector <Edge> ReadEdges(int64_t number_of_paths, std::istream& input) {
+std::vector <Edge> ReadEdges(int64_t number_of_edges, std::istream& input) {
     int64_t x, y;
     std::vector <Edge> edges;
-    for (int64_t i = 0; i < number_of_paths; ++i)
+    for (int64_t i = 0; i < number_of_edges; ++i)
     {
         input >> x >> y;
         Edge path;
-        path.room_from = x;
-        path.room_to = y;
+        path.vertex_from = x;
+        path.vertex_to = y;
         edges.push_back(path);
     }
     return edges;
@@ -132,11 +140,12 @@ int main() {
     std::cin.tie(nullptr);
 
     const auto &number_of_rooms = ReadNumber();
-    const auto &number_of_path = ReadNumber();
+    const auto & number_of_edges = ReadNumber();
     const auto &path_length = ReadNumber();
-    const auto &edges = ReadEdges(number_of_path);
-    const auto &variants = Run(number_of_rooms, number_of_path,
-                               path_length, edges);
+    const auto &edges = ReadEdges(number_of_edges);
+    const auto &variants = CountNumberOfVariants(number_of_rooms,
+                                                 number_of_edges,
+                                                 path_length, edges);
     Write(variants);
 
     return 0;
