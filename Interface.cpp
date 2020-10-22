36e670f45d3fc18ebeaee7a784f0caae03a8764d
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-10-22 19:45:32 +0300

    Update Interface.cpp

--- Interface.cpp
+++ Interface.cpp
@@ -7,12 +7,15 @@ const int64_t kMd = 1000000007;
 
 struct Edge
 {
-    int64_t room_from;
-    int64_t room_to;
+    int64_t vertex_from;
+    int64_t vertex_to;
 };
 
+// Reading numbers
+int64_t ReadNumber(std::istream& input = std::cin);
+
 // Creating and reading edges
-std::vector <Edge> ReadEdges(int64_t number_of_paths,
+std::vector <Edge> ReadEdges(int64_t number_of_edges,
                              std::istream& input = std::cin);
 
 // Writing a number of possible variants
@@ -24,8 +27,9 @@ class Matrix {
     int64_t size_;
  public:
     int64_t GetSize();
-    Matrix(int64_t number_of_rooms, int64_t digit);
-    void Create(std::vector <Edge> edges, int64_t number_of_path);
+    Matrix(int64_t matrix_size, int64_t digit);
+    Matrix(std::vector <Edge> edges, int64_t matrix_size,
+           int64_t number_of_edges);
     int64_t get(int64_t i, int64_t j);
     void set(int64_t i, int64_t j, int64_t value);
 };
@@ -37,15 +41,14 @@ Matrix MultiplicationOfMatrices(Matrix a, Matrix b);
 Matrix MatrixInPower(Matrix a, int64_t k);
 
 // The number of possible paths from room#0 with given length
-int64_t SumCalc(Matrix a, int64_t number_of_rooms);
+int64_t SumCalculation(Matrix matrix, int64_t matrix_size);
 
-int64_t Run(int64_t number_of_rooms, int64_t number_of_path,
+int64_t CountNumberOfVariants(int64_t number_of_rooms, int64_t number_of_edges,
             int64_t path_length, std::vector <Edge> edges);
 
-int64_t ReadNumber(std::istream& input = std::cin);
 int64_t ReadNumber(std::istream& input);
 
-std::vector <Edge> ReadEdges(int64_t number_of_paths, std::istream& input);
+std::vector <Edge> ReadEdges(int64_t number_of_edges, std::istream& input);
 
 void Write(int64_t variants, std::ostream& output);
 
@@ -54,11 +57,12 @@ int main() {
     std::cin.tie(nullptr);
 
     const auto &number_of_rooms = ReadNumber();
-    const auto &number_of_path = ReadNumber();
+    const auto & number_of_edges = ReadNumber();
     const auto &path_length = ReadNumber();
-    const auto &edges = ReadEdges(number_of_path);
-    const auto &variants = Run(number_of_rooms, number_of_path,
-                               path_length, edges);
+    const auto &edges = ReadEdges(number_of_edges);
+    const auto &variants = CountNumberOfVariants(number_of_rooms,
+                                                 number_of_edges,
+                                                 path_length, edges);
     Write(variants);
 
     return 0;
