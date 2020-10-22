1bce937c73d123cc10cc7eb073d456f7cf83ee96
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-10-22 18:54:11 +0300

    task 3 - code

--- CODE.cpp
+++ CODE.cpp
@@ -3,102 +3,141 @@
 #include <utility>
 #include <map>
 
-const int64_t md = 1000000007;
+const int64_t kMd = 1000000007;
+
+struct Edge
+{
+    int64_t room_from;
+    int64_t room_to;
+};
+
+// Creating and reading edges
+std::vector <Edge> ReadEdges(int64_t number_of_paths,
+                             std::istream& input = std::cin);
+
+// Writing a number of possible variants
+void Write(int64_t variants, std::ostream& output = std::cout);
 
 class Matrix {
  private:
-    std::vector < std::vector <int64_t> > matrix;
-    int64_t Size;
+    std::vector < std::vector <int64_t> > matrix_;
+    int64_t size_;
  public:
     int64_t GetSize();
-    Matrix(int64_t N, int64_t x);
-    void Read(int64_t M);
+    Matrix(int64_t number_of_rooms, int64_t digit);
+    void Create(std::vector <Edge> edges, int64_t number_of_path);
     int64_t get(int64_t i, int64_t j);
     void set(int64_t i, int64_t j, int64_t value);
 };
 
-// Getting the size of the Matrix
 int64_t Matrix::GetSize() {
-    return this->Size;
+    return this->size_;
 }
 
-// Creating a Matrix N*N with 0
-Matrix::Matrix(int64_t N, int64_t x) {
-    std::vector <int64_t> str(N, 0);
-    for (int64_t i = 0; i < N; ++i)
-        this->matrix.push_back(str);
-    for (int64_t i = 0; i < N; ++i)
-        this->matrix[i][i] = x;
-    this->Size = N;
+// Creating an identity matrix N*N
+Matrix::Matrix(int64_t number_of_rooms, int64_t digit) {
+    std::vector <int64_t> str(number_of_rooms, 0);
+    for (int64_t i = 0; i < number_of_rooms; ++i)
+        this->matrix_.push_back(str);
+    for (int64_t i = 0; i < number_of_rooms; ++i)
+        this->matrix_[i][i] = digit;
+    this->size_ = number_of_rooms;
 }
 
-// Reading the Matrix
-void Matrix::Read(int64_t M) {
+void Matrix::Create(std::vector <Edge> edges, int64_t number_of_path) {
     int64_t x, y;
-    for (int64_t i = 0; i < M; ++i) {
-        std::cin >> x >> y;
-        this->matrix[x - 1][y - 1]++;
+    for (int64_t i = 0; i < number_of_path; ++i) {
+        this->matrix_[edges[i].room_from - 1][edges[i].room_to - 1]++;
     }
 }
 
-// Getter
 int64_t Matrix::get(int64_t i, int64_t j) {
-    return this->matrix[i][j];
+    return this->matrix_[i][j];
 }
 
-// Setter
 void Matrix::set(int64_t i, int64_t j, int64_t value) {
-    this->matrix[i][j] = value;
+    this->matrix_[i][j] = value;
 }
 
 // Matrix Multiplication
-Matrix Multiplication(Matrix a, Matrix b) {
+Matrix MultiplicationOfMatrices(Matrix a, Matrix b) {
     int64_t N = a.GetSize();
     Matrix c(N, 0);
     for (int64_t i = 0; i < N; ++i)
         for (int64_t j = 0; j < N; ++j) {
             c.set(i, j, 0);
             for (int64_t k = 0; k < N; ++k)
-                c.set(i, j, (a.get(i, k) * b.get(k, j) % md +
-                      c.get(i, j)) % md);
+                c.set(i, j, (a.get(i, k) * b.get(k, j) % kMd +
+                      c.get(i, j)) % kMd);
         }
     return c;
 }
 
-// Matrix in Power
-Matrix InPower(Matrix a, int64_t k) {
+// Exponentiation of a matrix
+Matrix MatrixInPower(Matrix a, int64_t k) {
     int64_t N = a.GetSize();
     Matrix res(N, 1);
     while (k) {
         if (k & 1)
-            res = Multiplication(res, a);
-        a = Multiplication(a, a);
+            res = MultiplicationOfMatrices(res, a);
+        a = MultiplicationOfMatrices(a, a);
         k >>= 1;
     }
     return res;
 }
 
-// Calculating an answer for vertex 0
-int64_t Sum_calc(Matrix a, int64_t N) {
+// The number of possible paths from room#0 with given length
+int64_t SumCalc(Matrix a, int64_t number_of_rooms) {
     int64_t sum = 0;
-    for (int64_t i = 0; i < N; ++i)
-        sum = (sum + a.get(0, i)) % md;
+    for (int64_t i = 0; i < number_of_rooms; ++i)
+        sum = (sum + a.get(0, i)) % kMd;
     return sum;
 }
 
-// Reading and solving
-void Run() {
-    int64_t N, M, K;
-    std::cin >> N >> M >> K;
-    Matrix a(N, 0);
-    a.Read(M);
-    a = InPower(a, K);
-    std::cout << Sum_calc(a, N);
+int64_t Run(int64_t number_of_rooms, int64_t number_of_path,
+            int64_t path_length, std::vector <Edge> edges) {
+    Matrix rooms_matrix(number_of_rooms, 0);
+    rooms_matrix.Create(edges, number_of_path);
+    rooms_matrix = MatrixInPower(rooms_matrix, path_length);
+    return SumCalc(rooms_matrix, number_of_rooms);
+}
+
+int64_t ReadNumber(std::istream& input = std::cin);
+int64_t ReadNumber(std::istream& input) {
+    int value;
+    input >> value;
+    return value;
+}
+
+std::vector <Edge> ReadEdges(int64_t number_of_paths, std::istream& input) {
+    int64_t x, y;
+    std::vector <Edge> edges;
+    for (int64_t i = 0; i < number_of_paths; ++i)
+    {
+        input >> x >> y;
+        Edge path;
+        path.room_from = x;
+        path.room_to = y;
+        edges.push_back(path);
+    }
+    return edges;
+}
+
+void Write(int64_t variants, std::ostream& output) {
+    output << variants << '\n';
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    Run();
+
+    const auto &number_of_rooms = ReadNumber();
+    const auto &number_of_path = ReadNumber();
+    const auto &path_length = ReadNumber();
+    const auto &edges = ReadEdges(number_of_path);
+    const auto &variants = Run(number_of_rooms, number_of_path,
+                               path_length, edges);
+    Write(variants);
+
     return 0;
 }
