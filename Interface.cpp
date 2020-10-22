12e7a77f40a335e62bda4fecdb216ea208eb1a9f
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-10-22 21:21:39 +0300

    Update Interface.cpp

--- Interface.cpp
+++ Interface.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 #include <map>
 
-const int64_t kMod = 1000000007;
+const int64_t MODULE = 1000000007;
 
 struct Edge
 {
@@ -15,54 +15,60 @@ struct Edge
 int64_t ReadNumber(std::istream& input = std::cin);
 
 // Creating and reading edges
-std::vector <Edge> ReadEdges(int64_t number_of_edges,
-                             std::istream& input = std::cin);
+std::vector<Edge> ReadEdges(const int64_t &number_of_edges,
+    std::istream& input = std::cin);
 
 // Writing a number of possible variants
-void Write(int64_t variants, std::ostream& output = std::cout);
+void Write(const int64_t &variants, std::ostream& output = std::cout);
 
 class Matrix {
  private:
-    std::vector < std::vector <int64_t> > matrix_;
+    std::vector< std::vector<int64_t> > matrix_;
     int64_t size_;
  public:
-    int64_t GetSize();
-    Matrix(int64_t matrix_size, int64_t digit);
-    Matrix(std::vector <Edge> edges, int64_t matrix_size,
-           int64_t number_of_edges);
-    int64_t get(int64_t i, int64_t j);
-    void set(int64_t i, int64_t j, int64_t value);
+    int64_t GetSize() const;
+    Matrix(const int64_t& matrix_size, const int64_t& digit);
+    Matrix(const std::vector<Edge> &edges, const int64_t &matrix_size,
+           const int64_t &number_of_edges);
+    int64_t get(const int64_t & vertex_first,
+                const int64_t & vertex_second) const;
+    void set(const int64_t &vertex_first, const int64_t &vertex_second,
+             const int64_t &value);
 };
 
 // Matrix Multiplication
-Matrix MultiplicationOfMatrices(Matrix matrix_first, Matrix matrix_second);
+Matrix MultiplicationOfMatrices(const Matrix &matrix_first,
+                                const Matrix &matrix_second);
 
 // Exponentiation of a matrix
 Matrix MatrixInPower(Matrix matrix, int64_t power);
 
 // The number of possible paths from room#0 with given length
-int64_t SumCalculation(Matrix matrix, int64_t matrix_size);
+int64_t SumCalculation(const Matrix &matrix, const int64_t &matrix_size);
 
-int64_t CountNumberOfVariants(int64_t number_of_rooms, int64_t number_of_edges,
-            int64_t path_length, std::vector <Edge> edges);
+int64_t CountNumberOfVariants(const int64_t &number_of_rooms,
+                              const int64_t &number_of_edges,
+                              const int64_t &path_length,
+                              const std::vector<Edge> &edges);
 
 int64_t ReadNumber(std::istream& input);
 
-std::vector <Edge> ReadEdges(int64_t number_of_edges, std::istream& input);
+std::vector<Edge> ReadEdges(const int64_t &number_of_edges,
+                            std::istream& input);
 
-void Write(int64_t variants, std::ostream& output);
+void Write(const int64_t &variants, std::ostream& output);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    const auto &number_of_rooms = ReadNumber();
-    const auto & number_of_edges = ReadNumber();
-    const auto &path_length = ReadNumber();
-    const auto &edges = ReadEdges(number_of_edges);
-    const auto &variants = CountNumberOfVariants(number_of_rooms,
-                                                 number_of_edges,
-                                                 path_length, edges);
+    const auto& number_of_rooms = ReadNumber();
+    const auto& number_of_edges = ReadNumber();
+    const auto& path_length = ReadNumber();
+    const auto& edges = ReadEdges(number_of_edges);
+    const auto& variants = CountNumberOfVariants(number_of_rooms,
+        number_of_edges,
+        path_length, edges);
     Write(variants);
 
     return 0;
