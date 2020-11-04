fdb134baf8c7b96b7ec6571384cedeff48e5a244
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-11-04 13:10:20 +0200

    Update CODE.cpp

--- CODE.cpp
+++ CODE.cpp
@@ -5,15 +5,11 @@
 #include <algorithm>
 
 const int64_t MODULUS = 1000000007;
+const int64_t INDEX = 1;
 
 struct Edge {
     int64_t vertex_from;
     int64_t vertex_to;
-};
-
-struct MatrixSize {
-    int64_t number_of_strings;
-    int64_t number_of_columns;
 };
 
 // Reading numbers
@@ -29,40 +25,45 @@ void Write(const int64_t& ways, std::ostream& output = std::cout);
 class Matrix {
  private:
     std::vector< std::vector<int64_t> > matrix_;
-    MatrixSize size_;
+    int64_t number_of_strings_;
+    int64_t number_of_columns_;
 
  public:
-     Matrix(const MatrixSize& size, const bool& digit) {
-         std::vector< std::vector<int64_t> >
-             matrix(size.number_of_strings,
-                 std::vector<int64_t>(size.number_of_columns, 0));
+     Matrix(const int64_t& number_of_strings, const int64_t& number_of_columns,
+            const bool& digit) {
+         std::vector< std::vector<int64_t> > matrix(number_of_strings,
+                                                    std::vector<int64_t>
+                                                    (number_of_columns, 0));
          if (digit) {
-             if (size.number_of_strings != size.number_of_columns) {
+             if (number_of_strings != number_of_columns) {
                  std::cout << "Impossible to create an identity matrix";
              }
-             for (int i = 0; i < size.number_of_strings; ++i)
+             for (int i = 0; i < number_of_strings; ++i) {
                  matrix[i][i] = 1;
+             }
          }
          matrix_ = matrix;
-         size_ = size;
+         number_of_strings_ = number_of_strings;
+         number_of_columns_ = number_of_columns;
      }
 
      Matrix(const std::vector<Edge>& edges, const int64_t& size) {
-         std::vector< std::vector<int64_t> >
-             matrix(size,
-                 std::vector<int64_t>(size, 0));
+         std::vector< std::vector<int64_t> > matrix(size,
+                                                    std::vector<int64_t>
+                                                    (size, 0));
          for (int64_t i = 0; i < edges.size(); ++i) {
-             matrix[edges[i].vertex_from][edges[i].vertex_to]++;
+             matrix[edges[i].vertex_from - INDEX][edges[i].vertex_to -
+                                                  INDEX]++;
          }
          matrix_ = matrix;
-         size_.number_of_strings = size;
-         size_.number_of_columns = size;
+         number_of_strings_ = size;
+         number_of_columns_ = size;
      }
 
      int64_t Get(const int64_t& line,
                  const int64_t& column) const {
-         if (line < 0 || line >= size_.number_of_strings || column < 0 ||
-             column >= size_.number_of_columns) {
+         if (line < 0 || line >= number_of_strings_ || column < 0 ||
+             column >= number_of_columns_) {
              std::cout << "Indexes of matrix are out of range";
          }
          return matrix_[line][column];
@@ -71,50 +72,44 @@ class Matrix {
      void Set(const int64_t& line,
               const int64_t& column,
               const int64_t& value) {
-         if (line < 0 || line >= size_.number_of_strings || column < 0 ||
-             column >= size_.number_of_columns) {
+         if (line < 0 || line >= number_of_strings_ || column < 0 ||
+             column >= number_of_columns_) {
              std::cout << "Indexes of matrix are out of range";
          }
          matrix_[line][column] = value;
      }
 
-     MatrixSize GetSize() const {
-         return size_;
+     int64_t GetNumberOfStrings() const {
+         return number_of_strings_;
+     }
+
+     int64_t GetNumberOfColumns() const {
+         return number_of_columns_;
      }
 };
 
-bool MultiplicationPossible(const MatrixSize& size_of_left_matrix,
-                            const MatrixSize& size_of_right_matrix) {
-    const int64_t left_columns = size_of_left_matrix.number_of_strings;
-    const int64_t right_strings = size_of_right_matrix.number_of_columns;
-    if (left_columns == right_strings) {
-        return true;
-    }
-    return false;
-}
 // Matrix Multiplication
 Matrix MultiplicationOfMatrices(const Matrix& matrix_left,
                                 const Matrix& matrix_right) {
-    const MatrixSize size_of_left_matrix = matrix_left.GetSize();
-    const MatrixSize size_of_right_matrix = matrix_right.GetSize();
-    if (MultiplicationPossible(size_of_left_matrix,
-                               size_of_right_matrix) == false) {
+    const int64_t& strings_left_matrix = matrix_left.GetNumberOfStrings();
+    const int64_t& columns_left_matrix = matrix_left.GetNumberOfColumns();
+    const int64_t& strings_right_matrix = matrix_right.GetNumberOfStrings();
+    const int64_t& columns_right_matrix = matrix_right.GetNumberOfColumns();
+    if (columns_left_matrix != strings_right_matrix) {
         std::cout << "Multiplication of matrixes is impossible";
     }
-    MatrixSize size_result;
-    size_result.number_of_strings = size_of_left_matrix.number_of_strings;
-    size_result.number_of_columns = size_of_right_matrix.number_of_columns;
-    Matrix matrix_result(size_result, false);
-    for (int64_t i = 0; i < size_result.number_of_strings; ++i) {
-        for (int64_t j = 0; j < size_result.number_of_columns; ++j) {
-            for (int64_t k = 0; k < size_of_left_matrix.number_of_columns;
-                ++k) {
+    Matrix matrix_result(strings_left_matrix, columns_right_matrix, false);
+    for (int64_t i = 0; i < strings_left_matrix; ++i) {
+        for (int64_t j = 0; j < columns_right_matrix; ++j) {
+            for (int64_t k = 0; k < columns_left_matrix; ++k) {
                 const int64_t number_of_ways_in_left = matrix_left.Get(i, k);
                 const int64_t number_of_ways_in_right = matrix_right.Get(k, j);
                 const int64_t current_number_of_ways = matrix_result.Get(i, j);
                 const int64_t number_of_ways = ((number_of_ways_in_left *
-                    number_of_ways_in_right) % MODULUS +
-                    current_number_of_ways) % MODULUS;
+                                                 number_of_ways_in_right) %
+                                                 MODULUS +
+                                                 current_number_of_ways) %
+                                                 MODULUS;
                 matrix_result.Set(i, j, number_of_ways);
             }
         }
@@ -125,11 +120,12 @@ Matrix MultiplicationOfMatrices(const Matrix& matrix_left,
 // Exponentiation of a matrix
 Matrix MatrixInPower(const Matrix& matrix, int64_t power) {
     Matrix delta_matrix = matrix;
-    Matrix result_matrix(delta_matrix.GetSize(), true);
+    Matrix result_matrix(delta_matrix.GetNumberOfStrings(),
+                         delta_matrix.GetNumberOfColumns(), true);
     while (power) {
         if (power % 2 != 0) {
             result_matrix = MultiplicationOfMatrices(result_matrix,
-                delta_matrix);
+                                                     delta_matrix);
         }
         delta_matrix = MultiplicationOfMatrices(delta_matrix, delta_matrix);
         power /= 2;
@@ -167,14 +163,6 @@ std::vector<Edge> ReadEdges(const int64_t& number_of_edges,
     return edges;
 }
 
-std::vector<Edge> VertexIndexingOneToZero(std::vector<Edge> edges) {
-    for (int64_t i = 0; i < edges.size(); ++i) {
-        --edges[i].vertex_from;
-        --edges[i].vertex_to;
-    }
-    return edges;
-}
-
 void Write(const int64_t& ways, std::ostream& output) {
     output << ways << '\n';
 }
@@ -186,8 +174,7 @@ int main() {
     const auto& number_of_rooms = ReadNumber();
     const auto& number_of_edges = ReadNumber();
     const auto& path_length = ReadNumber();
-    const auto& edges_not_checked_indexing = ReadEdges(number_of_edges);
-    const auto& edges = VertexIndexingOneToZero(edges_not_checked_indexing);
+    const auto& edges = ReadEdges(number_of_edges);
     const auto& ways = CountNumberOfWays(number_of_rooms, path_length, edges);
     Write(ways);
 
