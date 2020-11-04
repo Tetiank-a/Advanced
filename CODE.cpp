cbbf0c74d872674a3fc559344d128f4f51fe1e34
Tetiana Rakova <58983291+Tetiank-a@users.noreply.github.com>
2020-11-04 09:43:51 +0200

    Update CODE.cpp

--- CODE.cpp
+++ CODE.cpp
@@ -16,7 +16,7 @@ int64_t ReadNumber(std::istream& input = std::cin);
 
 // Creating and reading edges
 std::vector<Edge> ReadEdges(const int64_t& number_of_edges,
-    std::istream& input = std::cin);
+                            std::istream& input = std::cin);
 
 // Writing a number of possible variants
 void Write(const int64_t& ways, std::ostream& output = std::cout);
@@ -25,74 +25,69 @@ class Matrix {
  private:
     std::vector< std::vector<int64_t> > matrix_;
     int64_t size_;
+
  public:
-    int64_t GetSize() const;
-    Matrix(const int64_t& matrix_size, const bool& digit);
-    Matrix(const std::vector<Edge>& edges, const int64_t& matrix_size);
-    int64_t get(const int64_t& line,
-        const int64_t& column) const;
-    void set(const int64_t& line, const int64_t& column,
-        const int64_t& value);
+     // Creating an identity matrix N*N, digit - 0 or 1
+     Matrix(const int64_t& matrix_size, const bool& digit) {
+         const std::vector<int64_t> str(matrix_size, 0);
+         for (int64_t i = 0; i < matrix_size; ++i)
+             this->matrix_.push_back(str);
+         for (int64_t i = 0; i < matrix_size; ++i)
+             this->matrix_[i][i] = int64_t(digit);
+         this->size_ = matrix_size;
+     }
+
+     Matrix(const std::vector<Edge>& edges,
+            const int64_t& matrix_size) {
+         this->size_ = matrix_size;
+         std::vector<int64_t> str(matrix_size, 0);
+         for (int64_t i = 0; i < matrix_size; ++i)
+             this->matrix_.push_back(str);
+
+         for (int64_t i = 0; i < edges.size(); ++i) {
+             this->matrix_[edges[i].vertex_from][edges[i].vertex_to]++;
+         }
+     }
+
+     int64_t Get(const int64_t& line,
+                 const int64_t& column) const {
+         if (line < 0 || line >= matrix_.size() || column < 0 ||
+             column >= matrix_[matrix_.size() - 1].size()) {
+             std::cout << "Indexes of matrix are out of range";
+         }
+         return this->matrix_[line][column];
+     }
+
+     void Set(const int64_t& line,
+              const int64_t& column,
+              const int64_t& value) {
+         if (line < 0 || line >= matrix_.size() || column < 0 ||
+             column >= matrix_[matrix_.size() - 1].size()) {
+             std::cout << "Indexes of matrix are out of range";
+         }
+         this->matrix_[line][column] = value;
+     }
+
+     int64_t GetSize() const {
+         return this->size_;
+     }
 };
-
-int64_t Matrix::GetSize() const {
-    return this->size_;
-}
-
-// Creating an identity matrix N*N, digit - 0 or 1
-Matrix::Matrix(const int64_t& matrix_size, const bool& digit) {
-    const std::vector<int64_t> str(matrix_size, 0);
-    for (int64_t i = 0; i < matrix_size; ++i)
-        this->matrix_.push_back(str);
-    for (int64_t i = 0; i < matrix_size; ++i)
-        this->matrix_[i][i] = int64_t(digit);
-    this->size_ = matrix_size;
-}
-
-Matrix::Matrix(const std::vector<Edge>& edges, const int64_t& matrix_size) {
-    this->size_ = matrix_size;
-    std::vector<int64_t> str(matrix_size, 0);
-    for (int64_t i = 0; i < matrix_size; ++i)
-        this->matrix_.push_back(str);
-
-    for (int64_t i = 0; i < edges.size(); ++i) {
-        this->matrix_[edges[i].vertex_from - 1][edges[i].vertex_to - 1]++;
-    }
-}
-
-int64_t Matrix::get(const int64_t& line,
-    const int64_t& column) const {
-    if (line < 0 || line >= matrix_.size() || column < 0 ||
-        column >= matrix_[matrix_.size() - 1].size()) {
-        std::cout << "indexes of matrix are out of range";
-    }
-    return this->matrix_[line][column];
-}
-
-void Matrix::set(const int64_t& line, const int64_t& column,
-    const int64_t& value) {
-    if (line < 0 || line >= matrix_.size() || column < 0 ||
-        column >= matrix_[matrix_.size() - 1].size()) {
-        std::cout << "indexes of matrix are out of range";
-    }
-    this->matrix_[line][column] = value;
-}
 
 // Matrix Multiplication
 Matrix MultiplicationOfMatrices(const Matrix& matrix_left,
-    const Matrix& matrix_right) {
+                                const Matrix& matrix_right) {
     const int64_t matrix_size = matrix_left.GetSize();
     Matrix matrix_result(matrix_size, 0);
     for (int64_t i = 0; i < matrix_size; ++i)
         for (int64_t j = 0; j < matrix_size; ++j) {
             for (int64_t k = 0; k < matrix_size; ++k) {
-                int64_t number_of_ways_in_left = matrix_left.get(i, k);
-                int64_t number_of_ways_in_right = matrix_right.get(k, j);
-                int64_t current_number_of_ways = matrix_result.get(i, j);
-                int64_t number_of_ways = ((number_of_ways_in_left *
-                                          number_of_ways_in_right) % MODULUS +
-                                          current_number_of_ways) % MODULUS;
-                matrix_result.set(i, j, number_of_ways);
+                const int64_t number_of_ways_in_left = matrix_left.Get(i, k);
+                const int64_t number_of_ways_in_right = matrix_right.Get(k, j);
+                const int64_t current_number_of_ways = matrix_result.Get(i, j);
+                const int64_t number_of_ways = ((number_of_ways_in_left *
+                    number_of_ways_in_right) % MODULUS +
+                    current_number_of_ways) % MODULUS;
+                matrix_result.Set(i, j, number_of_ways);
             }
         }
     return matrix_result;
@@ -113,13 +108,13 @@ Matrix MatrixInPower(const Matrix& matrix, int64_t power) {
 }
 
 int64_t CountNumberOfWays(const int64_t& number_of_rooms,
-    const int64_t& path_length,
-    const std::vector<Edge>& edges) {
+                          const int64_t& path_length,
+                          const std::vector<Edge>& edges) {
     Matrix rooms_matrix(edges, number_of_rooms);
     rooms_matrix = MatrixInPower(rooms_matrix, path_length);
     int64_t sum = 0;
     for (int64_t i = 0; i < number_of_rooms; ++i)
-        sum = (sum + rooms_matrix.get(0, i)) % MODULUS;
+        sum = (sum + rooms_matrix.Get(0, i)) % MODULUS;
     return sum;
 }
 
@@ -130,13 +125,21 @@ int64_t ReadNumber(std::istream& input) {
 }
 
 std::vector<Edge> ReadEdges(const int64_t& number_of_edges,
-    std::istream& input) {
+                            std::istream& input) {
     std::vector<Edge> edges;
     for (int64_t i = 0; i < number_of_edges; ++i)
     {
         Edge path;
         input >> path.vertex_from >> path.vertex_to;
         edges.push_back(path);
+    }
+    return edges;
+}
+
+std::vector<Edge> VertexIndexingOneToZero(std::vector<Edge> edges) {
+    for (int64_t i = 0; i < edges.size(); ++i) {
+        --edges[i].vertex_from;
+        --edges[i].vertex_to;
     }
     return edges;
 }
@@ -152,7 +155,8 @@ int main() {
     const auto& number_of_rooms = ReadNumber();
     const auto& number_of_edges = ReadNumber();
     const auto& path_length = ReadNumber();
-    const auto& edges = ReadEdges(number_of_edges);
+    const auto& edges_not_checked_indexing = ReadEdges(number_of_edges);
+    const auto& edges = VertexIndexingOneToZero(edges_not_checked_indexing);
     const auto& ways = CountNumberOfWays(number_of_rooms,
         path_length, edges);
     Write(ways);
